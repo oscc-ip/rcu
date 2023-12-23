14acba7d60094555db58bea7f987e19818e47cd7
maksyuki <maksyuki@126.com>
2023-12-23 14:47:04 +0800

    feat: add comp script and tb files

---
+++ tb/rcu_test.sv
@@ -0,0 +1,53 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// rcu is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`ifndef INC_PWM_TEST_SV
+`define INC_PWM_TEST_SV
+
+`include "apb4_master.sv"
+`include "rcu_define.sv"
+
+class RCUTest extends APB4Master;
+  string                 name;
+  int                    wr_val;
+  virtual apb4_if.master apb4;
+  virtual rcu_if.tb      rcu;
+
+  extern function new(string name = "rcu_test", virtual apb4_if.master apb4, virtual rcu_if.tb rcu);
+  extern task automatic test_reset_reg();
+  extern task automatic test_wr_rd_reg(input bit [31:0] run_times = 1000);
+endclass
+
+function RCUTest::new(string name, virtual apb4_if.master apb4, virtual rcu_if.tb rcu);
+  super.new("apb4_master", apb4);
+  this.name   = name;
+  this.wr_val = 0;
+  this.apb4   = apb4;
+  this.rcu    = rcu;
+endfunction
+
+task automatic RCUTest::test_reset_reg();
+  super.test_reset_reg();
+  // verilog_format: off
+  this.rd_check(`RCU_CTRL_ADDR, "CTRL REG", 32'b0 & {`RCU_CTRL_WIDTH{1'b1}}, Helper::EQUL, Helper::INFO);
+  this.rd_check(`RCU_STAT_ADDR, "STAT REG", 32'd1 & {`RCU_STAT_WIDTH{1'b1}}, Helper::EQUL, Helper::INFO);
+  // verilog_format: on
+endtask
+
+task automatic RCUTest::test_wr_rd_reg(input bit [31:0] run_times = 1000);
+  super.test_wr_rd_reg();
+  // verilog_format: off
+  for (int i = 0; i < run_times; i++) begin
+    this.wr_rd_check(`RCU_CTRL_ADDR, "CTRL REG", $random & {`RCU_CTRL_WIDTH{1'b1}}, Helper::EQUL);
+  end
+  // verilog_format: on
+endtask
+
+`endif
---
+++ tb/test_top.sv
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// rcu is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`include "apb4_if.sv"
+`include "helper.sv"
+`include "rcu_define.sv"
+
+program automatic test_top (
+    apb4_if.master apb4,
+    rcu_if.tb   rcu
+);
+
+  string wave_name = "default.fsdb";
+  task sim_config();
+    $timeformat(-9, 1, "ns", 10);
+    if ($test$plusargs("WAVE_ON")) begin
+      $value$plusargs("WAVE_NAME=%s", wave_name);
+      $fsdbDumpfile(wave_name);
+      $fsdbDumpvars("+all");
+    end
+  endtask
+
+  RCUTest rcu_hdl;
+
+  initial begin
+    Helper::start_banner();
+    sim_config();
+    @(posedge apb4.presetn);
+    Helper::print("tb init done");
+    rcu_hdl = new("rcu_test", apb4, rcu);
+    rcu_hdl.init();
+    rcu_hdl.test_reset_reg();
+    rcu_hdl.test_wr_rd_reg();
+
+    Helper::end_banner();
+    #20000 $finish;
+  end
+
+endprogram
